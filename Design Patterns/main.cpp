a78d3b9262e1a8cf4ab9da86e2d1788511492ba4
hchaudhari <hchaudhari@digitalgurdian.com>
2018-12-14 08:45:54 +0530

    Initial Commit

---
+++ Design Patterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Design Patterns
+//
+//  Created by Hrishikesh Chaudhari on 14/12/18.
+//  Copyright © 2018 Hrishikesh Chaudhari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
